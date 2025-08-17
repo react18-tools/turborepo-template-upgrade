65308d6fbca8371b5e561dfa462f5e83637b36d3
Mayank <mayank.srmu@gmail.com>
2025-08-17 16:39:53 +0530

    Add logging for last template repo commit and completion of diff operation in upgradeTemplate function

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -5,8 +5,8 @@ import { resolve } from "path";
 export const upgradeTemplate = (lastTemplateRepoCommit?: string) => {
   // Check if git tree is clean
   try {
-    execSync("git diff --quiet", { encoding: "utf8" });
-    execSync("git diff --cached --quiet", { encoding: "utf8" });
+    execSync("git diff --quiet", { encoding: "utf8", stdio: "inherit" });
+    execSync("git diff --cached --quiet", { encoding: "utf8", stdio: "inherit" });
   } catch {
     console.error("Error: Please commit or stash your changes before upgrading.");
     process.exit(1);
@@ -15,6 +15,7 @@ export const upgradeTemplate = (lastTemplateRepoCommit?: string) => {
   try {
     execSync("git remote add template https://github.com/react18-tools/turborepo-template", {
       encoding: "utf8",
+      stdio: "inherit",
     });
   } catch {
     // ignore
@@ -30,11 +31,13 @@ export const upgradeTemplate = (lastTemplateRepoCommit?: string) => {
     if (!lastTemplateRepoCommit) {
       throw new Error("Last Turborepo commit not found");
     }
-    execSync("git fetch template", { encoding: "utf8" });
+    console.log({ lastTemplateRepoCommit });
+    execSync("git fetch template", { encoding: "utf8", stdio: "inherit" });
     execSync(
       `git diff ${lastTemplateRepoCommit} template/main -- . ':!docs' ':!lib' > .template.patch`,
-      { encoding: "utf8" },
+      { encoding: "utf8", stdio: "inherit" },
     );
+    console.log("diff done!");
   } catch (err) {
     console.error(err);
   }
