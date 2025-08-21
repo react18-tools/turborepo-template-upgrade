a1dc86b6f3208cd2bb9ec6b14e7454f48416c6e0
Mayank <mayank.srmu@gmail.com>
2025-08-21 20:49:48 +0530

    docs(changeset): :! <- fix exclusion syntax

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -26,7 +26,7 @@ const createAndApplyPatch = (lastTemplateRepoCommit: string, exclusions: string[
       ?.split?.("\n")
       .filter((line: string) => line.startsWith("error"));
     errorLines.forEach((line: string) => {
-      exclusions.push(line.split(":")[1].trim());
+      exclusions.push(`:!${line.split(":")[1].trim()}`);
     });
     errorLogs.push("Applied patch with errors: ");
     errorLogs.push({ errorLines, exclusions });
