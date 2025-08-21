eeb85c43020831f566d301c896da1fb965519484
Mayank <mayank.srmu@gmail.com>
2025-08-21 20:30:53 +0530

    docs(changeset): fix: Simplify CLI template upgrade invocation by removing argument check - ensure default commit id is used

---
+++ .changeset/swift-kiwis-do.md
@@ -0,0 +1,5 @@
+---
+"turborepo-template-upgrade": patch
+---
+
+fix: Simplify CLI template upgrade invocation by removing argument check - ensure default commit id is used
--- lib/src/cli.ts
+++ lib/src/cli.ts
@@ -1,5 +1,3 @@
 import { upgradeTemplate } from ".";
 
-if (process.argv.length > 2) {
-  upgradeTemplate(...process.argv.slice(2));
-}
+upgradeTemplate(...process.argv.slice(2));
