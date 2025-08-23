0d5a669324f65f05377ac0024fcfadb41b6e6193
Mayank <mayank.srmu@gmail.com>
2025-08-23 15:33:50 +0530

    Update test

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -3,12 +3,13 @@ import { upgradeTemplate } from ".";
 import lstCommit from "../../.turborepo-template.lst?raw";
 import { rmSync } from "fs";
 import { resolve } from "path";
+import { getBaseCommit } from "./utils";
 
 describe("upgrade", () => {
   test("smoke", async ({ expect }) => {
     await Promise.all([upgradeTemplate(lstCommit.trim()), upgradeTemplate()]);
     rmSync(resolve(process.cwd(), "./.turborepo-template.lst"));
-    upgradeTemplate();
+    getBaseCommit();
     expect(true).toBe(true);
   });
 });
