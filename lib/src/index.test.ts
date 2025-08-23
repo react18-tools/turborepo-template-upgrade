44015c795af8744b871846bc174e844785b8ffc3
Mayank <mayank.srmu@gmail.com>
2025-08-23 15:49:59 +0530

    Restore after testing is done

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -4,12 +4,14 @@ import lstCommit from "../../.turborepo-template.lst?raw";
 import { rmSync } from "fs";
 import { resolve } from "path";
 import { getBaseCommit } from "./utils";
+import { execSync } from "child_process";
 
 describe("upgrade", () => {
   test("smoke", async ({ expect }) => {
     await Promise.all([upgradeTemplate(lstCommit.trim()), upgradeTemplate()]);
     rmSync(resolve(process.cwd(), "./.turborepo-template.lst"));
     getBaseCommit();
+    execSync("git restore .");
     expect(true).toBe(true);
   });
 });
