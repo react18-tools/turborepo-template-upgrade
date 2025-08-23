451692db4b9d0a2664c066928398b195066b263f
Mayank <mayank.srmu@gmail.com>
2025-08-23 15:14:14 +0530

    Update test

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -1,10 +1,13 @@
 import { describe, test } from "vitest";
 import { upgradeTemplate } from ".";
 import lstCommit from "../../.turborepo-template.lst?raw";
+import { execSync } from "child_process";
 
 describe("upgrade", () => {
   test("smoke", ({ expect }) => {
     upgradeTemplate(lstCommit.trim());
+    upgradeTemplate();
+    execSync("rm ../.turborepo-template.lst");
     upgradeTemplate();
     expect(true).toBe(true);
   });
