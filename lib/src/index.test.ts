231757e42c27060c957a6a09031d5bb5e579a700
Mayank <mayank.srmu@gmail.com>
2025-08-28 09:25:09 +0530

    Extend test timeout

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -5,12 +5,16 @@ import { getBaseCommit } from "./utils";
 import { execSync } from "child_process";
 
 describe("upgrade", () => {
-  test("smoke", async ({ expect }) => {
-    await upgradeTemplate(lstCommit.trim());
-    getBaseCommit();
-    execSync("git reset --hard HEAD");
-    // await upgradeTemplate();
-    // execSync("git reset --hard HEAD");
-    expect(true).toBe(true);
-  });
+  test(
+    "smoke",
+    async ({ expect }) => {
+      await upgradeTemplate(lstCommit.trim());
+      getBaseCommit();
+      execSync("git reset --hard HEAD");
+      await upgradeTemplate();
+      execSync("git reset --hard HEAD");
+      expect(true).toBe(true);
+    },
+    { timeout: 10000 },
+  );
 });
