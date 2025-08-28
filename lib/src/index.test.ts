2c954e844cb9955c762fb5d880bccaad844b8b1e
Mayank <mayank.srmu@gmail.com>
2025-08-28 09:06:15 +0530

    Update tests

--- lib/src/index.test.ts
+++ lib/src/index.test.ts
@@ -7,10 +7,12 @@ import { execSync } from "child_process";
 describe("upgrade", () => {
   test("smoke", async ({ expect }) => {
     await upgradeTemplate(lstCommit.trim());
-    // getBaseCommit();
-    // execSync("git restore --stage .");
-    // execSync("git restore .");
-    // await upgradeTemplate();
+    getBaseCommit();
+    execSync("git restore --stage .");
+    execSync("git restore .");
+    await upgradeTemplate();
+    execSync("git restore --stage .");
+    execSync("git restore .");
     expect(true).toBe(true);
   });
 });
